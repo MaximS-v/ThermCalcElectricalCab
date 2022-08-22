801d752e22e1e4746b8b51e80459e27038eba5c3
Max Safronov <bmc938@gmail.com>
2022-08-22 11:36:07 +0300

    Добавьте файлы проекта.

---
+++ ThermCalcElectricalCab/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using ThermCalcElectricalCab.Model;
+
+namespace ThermCalcElectricalCab
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly ElectricalCabinet _electricalCabinet;
+        private readonly OutsideTemperature _outsideTemperature;
+
+        public App()
+        {
+            _electricalCabinet = new ElectricalCabinet();
+            _outsideTemperature = new OutsideTemperature();
+        }
+    }
+}
---
+++ ThermCalcElectricalCab/Model/ElectricalCabinet.cs
@@ -0,0 +1,23 @@
+﻿using static ElCalcLib.ThermalCalcs;
+
+namespace ThermCalcElectricalCab.Model
+{
+    /// <summary>
+    /// Defines an electrical panel in terms of its thermal characteristics
+    /// </summary>
+    public class ElectricalCabinet
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+
+        public double HeatTransferCoeff { get; set; }
+
+        public ElCabsLayout Layout { get; set; }
+
+        public double ComponentsPower { get; set; }
+
+        public double MaxInTemp { get; set; }
+        public double MinInTemp { get; set; }
+    }
+}
