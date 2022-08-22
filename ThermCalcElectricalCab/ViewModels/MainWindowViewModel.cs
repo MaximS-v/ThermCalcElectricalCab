a01c2cd8a0ce37e525d485f978973473541fe8fe
Max Safronov <bmc938@gmail.com>
2022-08-22 16:42:09 +0300

    Get values from comboboxes

--- ThermCalcElectricalCab/ViewModels/MainWindowViewModel.cs
+++ ThermCalcElectricalCab/ViewModels/MainWindowViewModel.cs
@@ -15,29 +15,32 @@ namespace ThermCalcElectricalCab.ViewModels
         {
             get => _title;
             set => Set(ref _title, value);
-        } 
+        }
         #endregion
+
+        private double _selectedHeatTransferCoeff;
+        public double SelectedHeatTransferCoeff
+        {
+            get => _selectedHeatTransferCoeff;
+            set
+            {
+                Set(ref _selectedHeatTransferCoeff, value);
+            }
+        }
 
         public static Dictionary<string, double> HeatTransferCoeff => ThermalCalcs.HeatTransferCoeff;
 
         private ThermalCalcs.ElCabsLayout _selectedElCabsLayout;
         public ThermalCalcs.ElCabsLayout SelectedElCabsLayout
         {
-            get { return _selectedElCabsLayout; }
-            set 
-            { 
-                _selectedElCabsLayout = value;
-                OnPropertyChanged("SelectedElCabsLayout");
+            get => _selectedElCabsLayout;
+            set
+            {
+                Set(ref _selectedElCabsLayout, value);
             }
         }
 
-        public IEnumerable<ThermalCalcs.ElCabsLayout> ElCabsLayoutsValues
-        {
-            get
-            {
-                return Enum.GetValues(typeof(ThermalCalcs.ElCabsLayout)).Cast<ThermalCalcs.ElCabsLayout>();
-            }
-        }
+        public IEnumerable<ThermalCalcs.ElCabsLayout> ElCabsLayoutsValues => Enum.GetValues(typeof(ThermalCalcs.ElCabsLayout)).Cast<ThermalCalcs.ElCabsLayout>();
 
         private ElectricalCabinet _electricalCabinet;
         public ElectricalCabinet ElectricalCabinet { get => _electricalCabinet; }
